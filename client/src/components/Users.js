c2d94c340077377c97ace531585655e1a0525d4d
Heather Nuffer <zzzuzz@gmail.com>
2020-01-29 13:51:00 -0700

    App displays users from user's department after registering

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 
 const Users = props => {
     const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('http://localhost:5000/api/users')
+            .then(response => {
+                console.log(response);
+                setUsers(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
     return (
-        <div>Users go here</div>
+        <div>
+            {users && <h2>{users[0].department.toUpperCase()}</h2>}
+            {users && users.map(user => {
+                return (
+                    <div key={user.id}>
+                        <h3>{user.username}</h3>
+                        <p>ID: {user.id}</p>
+                        <p>department: {user.department}</p>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
